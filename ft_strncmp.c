74ad878721875aaa95bb7800d0ef2c4a451b645f
Imane El mohalil <elmohalilimane@gmail.com>
2022-03-31 16:18:53 +0000

    taha

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iel-moha <iel-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/17 08:23:13 by iel-moha          #+#    #+#             */
-/*   Updated: 2022/03/31 15:16:33 by iel-moha         ###   ########.fr       */
+/*   Created: 2021/11/07 17:13:49 by iel-moha          #+#    #+#             */
+/*   Updated: 2022/03/31 15:21:31 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
 
-#ifndef PIPEX_H
-# define PIPEX_H
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-
-#endif
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1 && s1[i] == s2[i])
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
--- pipex.c
+++ pipex.c
@@ -6,47 +6,75 @@
 /*   By: iel-moha <iel-moha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/17 08:23:00 by iel-moha          #+#    #+#             */
-/*   Updated: 2022/03/23 18:02:10 by iel-moha         ###   ########.fr       */
+/*   Updated: 2022/03/31 16:17:01 by iel-moha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void error_handling(int i, char *str)
+{
+	if(i == -1)
+	{
+		perror(str);
+		exit(1);
+	}	
+}
+
+void	ft_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(*str);
+}
+
+char **path_finder(char **env)
+{
+	int i;
+	char **path;
+	i = 0;
+	while(env[i])
+	{
+		if(ft_strncmp("PATH=", env[i] , 5) == 0)
+		{
+			path = ft_split(&env[i][5], ':');
+			return (path);		
+		}
+		i++;
+	}
+	return 0;
+}
+
 void cmd_execution(char *av,char **env)
 {
-	char *cmd;
-	char **argVec;
-	char **path;
-	path = ft_split(&env[6][5] , ':');
-	argVec = ft_split(av, ' ');
-	int i;
+	char	*cmd;
+	char	**argvec;
+	char	**path;	
+	int		i;
+
+	path = path_finder(env);
+	if(path == 0)
+		error_handling(-1, "Path not found");
+	argvec = ft_split(av, ' ');
 	i = 0;
 	while(path[i])
 	{
 		cmd = ft_strjoin(path[i], "/");
-		cmd = ft_strjoin(cmd, argVec[0]);
+		cmd = ft_strjoin(cmd, argvec[0]);
 		if((access(cmd, F_OK) == 0) && (access(cmd, X_OK) == 0))
-		{
-			execve(cmd, argVec, env);
-			//close(file);
-			//close(fd[0]);
-			//close(fd[1]);
-			perror("Error: Could not execute execve");
-			exit(1);
-		}
-		else
-			printf("Command not found.\n");
+			error_handling(execve(cmd, argvec, env), "Execve could not execute.");
 		i++;
+		free(cmd);
 	}
-}
-
-void error_handling(int i)
-{
-	if(i == -1)
-	{
-		perror("Error, action failed.");
-		exit(1);
-	}	
+	ft_free(path);
+	ft_free(argvec);
+	perror("Command not found.\n");
 }
 
 int main(int ac, char **av, char **env)
@@ -55,28 +83,34 @@ int main(int ac, char **av, char **env)
 	{
 		int file[2];
 		int fd[2];
+
 		pid_t pid[2];
-		error_handling(pipe(fd));
+		error_handling(pipe(fd),"Could not pipe");
 		pid[0] = fork();
-		error_handling(pid[0] == -1);
+		error_handling(pid[0], "Could not fork");
 		if(pid[0] == 0)
 		{
-			file[0] = open(av[1], O_RDONLY | O_CREAT, 0777);
-			error_handling(file[0]);
-			error_handling(dup2(fd[1], 1));
-			error_handling(dup2(file[0], 0));
+			close(fd[0]);
+			file[0] = open(av[1], O_RDONLY, 0777);
+			error_handling(file[0], "File could not open");
+			error_handling(dup2(fd[1], 1), "Dup2 failed");
+			error_handling(dup2(file[0], 0), "Dup2 failed");
 			cmd_execution(av[2], env);
 		}
 		pid[1] = fork();
-		error_handling(pid[1] == -1);
+		error_handling(pid[1], "Could not second fork");
 		if(pid[1] == 0)
-		{
+		{	
 			close(fd[1]);
 			file[1] = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-			error_handling(file[1]);
-			error_handling(dup2(file[1], 1));
-			error_handling(dup2(fd[0], 0));
+			error_handling(file[1], "File could not open");
+			error_handling(dup2(file[1], 1), "Dup2 failed");
+			error_handling(dup2(fd[0], 0), "Dup2 failed.");
 			cmd_execution(av[3], env);
 		}
+		close(fd[0]);
+		close(fd[1]);
+		wait(NULL);
+		wait(NULL);
 	}
 }
